29b935c55b527475034a7b0b001db3fc109ba74b
Simon <fiewind1@gmail.com>
2024-02-18 18:48:19 +0200

    Fixing Vercel deployment 2

--- components/SeasonalProducts.js
+++ components/SeasonalProducts.js
@@ -9,7 +9,7 @@ const SeasonalProducts = ({seasonalProducts}) => {
   return (
     <Paper className='featured-container' elevation={0}>
         <div className='featured-icon-container'>
-          <Typography variant='h3' component='h1' className='featured-heading'> This season's finest</Typography>
+          <Typography variant='h3' component='h1' className='featured-heading'> This season&apos;s finest</Typography>
           <img src="https://i.ibb.co/1fCHLBR/sakura.png" alt="flower-icon" />
         </div>
         <Typography variant='h6' component='h2' className='featured-sub-heading'> Hand picked seasonal selections </Typography>
