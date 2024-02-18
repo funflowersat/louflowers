ce18f0053acabc2b58b15339c92ee571e84cd238
Simon <fiewind1@gmail.com>
2024-02-18 18:45:05 +0200

    Fixing Vercel deployment

--- components/IconsSection.js
+++ components/IconsSection.js
@@ -13,11 +13,11 @@ function IconsSection() {
             </Typography>
             <Typography className='section-text-subtitle' variant='subtitle1' component='p'>
               
-              Indulge in a delightful surprise with our special offer! <span style={{fontWeight: '500'}}> When you spend over 110 euros </span> on our enchanting floral bouquets, you'll receive a complimentary mystery chocolate box to sweeten your experience.
+              Indulge in a delightful surprise with our special offer! <span style={{fontWeight: '500'}}> When you spend over 110 euros </span> on our enchanting floral bouquets, you&apos;ll receive a complimentary mystery chocolate box to sweeten your experience.
               <br></br> <span style={{marginTop: '7px'}}></span> 
               Immerse yourself in the joy of gifting or treating yourself to our exquisite blooms, and let us add an extra touch of decadence with our carefully curated selection of premium chocolates. Handpicked for their exceptional quality and irresistible flavors, our mystery chocolate boxes are a tantalizing addition to any order.
               <br></br> <span style={{marginTop: '7px'}}></span> 
-              <span id='section-extra-text'>Whether you're celebrating a milestone, expressing love and appreciation, or simply craving a moment of indulgence, our free chocolate box is the perfect complement to your bouquet purchase. It's our way of saying thank you for choosing our boutique for your floral needs.</span>
+              <span id='section-extra-text'>Whether you&apos;re celebrating a milestone, expressing love and appreciation, or simply craving a moment of indulgence, our free chocolate box is the perfect complement to your bouquet purchase. It&apos;s our way of saying thank you for choosing our boutique for your floral needs.</span>
               
             </Typography>
             <Link href='/store' className='section-button'><Typography variant='h6'> Shop Now </Typography></Link>
