2c164e30cc7471ed45d9e558fe5ef6e4f605d30f
funflowersat <160250766+funflowersat@users.noreply.github.com>
2025-05-18 20:06:45 +0300

    Update StateContext.js

--- context/StateContext.js
+++ context/StateContext.js
@@ -2,7 +2,7 @@ import React, { createContext, useState, useContext, useEffect} from 'react';
 import { toast } from 'react-hot-toast';
 
 const Context = createContext();
-
+ 
 export const StateContext = ({ children }) => {
     const [showCart, setShowCart] = useState(false);
     const [cartItems, setCartItems] = useState([]);
